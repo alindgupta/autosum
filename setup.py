59d47e83e5ec92cc2f28576f430865ebc826011b
Alind Gupta <alindgupta19@gmail.com>
2018-03-20 17:07:06 -0400

    Update setup and dependancies

--- setup.py
+++ setup.py
@@ -1,2 +1,18 @@
-import os
-import sys
+from setuptools import setup
+
+setup(name='autosum',
+      version='0.1',
+      description='Generate context-based embeddings of scientific literature',
+      url='http://github.com/alingupta/autosum',
+      author='Alind Gupta',
+      license='MIT',
+      packages=['autosum'],
+      install_requires=[
+          'requests',
+          'bs4',
+          'tensorflow',
+          'matplotlib',
+          'numpy',
+          'nltk'
+      ],
+      include_package_data=True)
