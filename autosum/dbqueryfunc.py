2f3187d74133ea6ca0b6d701f62db0ea0ea56419
Alind Gupta <alindgupta19@gmail.com>
2018-03-23 21:56:11 -0400

    Use multiprocessing and update docstrings

--- autosum/dbqueryfunc.py
+++ autosum/dbqueryfunc.py
@@ -1,176 +1,202 @@
+# -*- coding: utf-8 -*-
+
+"""dbquery.py
+
+This module uses the NCBI E-utilities to retrieve abstracts
+and saves them to a file. Multiple processes can be used to
+parallelize web scraping.
+
+Requires Python 3.6 or higher.
+
+Example:
+        $ python3 dbquery.py -f "summary.txt" -q "breast cancer BRCA1" -n 4
+
+Attributes:
+     DbQuery (class): Invoke the ``run`` method after initialization.
+         Some queries can take a while to download due to the large number
+         of studies (eg. "cancer"). It is often useful to qualify queries
+         to make them less generic (eg. "breast cancer BRCA1").
+
+Todo:
+    * Command line parsing.
+    * Stream instead of bulk download to save memory usage.
+    * Pipe parsing and tokenization to reduce IO time.
+
+"""
+import sys
+import time
 import argparse
-import datetime
 import functools
+import multiprocessing as mp
+from typing import Iterable, Union, Dict, List, IO
 import requests
 from bs4 import BeautifulSoup
-from multiprocessing import Pool, Queue
-from multithreading import Thread
 
-url_base = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
-esearch_base = url_base + 'esearch.fcgi?'
-efetch_base = url_base + 'efetch.fcgi?'
+assert sys.version_info >= (3, 6)
+
+# constants for E-utilities
+BASE_URL = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
+BASE_ESEARCH_URL = BASE_URL + 'esearch.fcgi?'
+BASE_EFETCH_URL = BASE_URL + 'efetch.fcgi?'
 
 
-def process_request(url, tags):
-    """ Scrape a webpage and extract contents of an html tag.
+def process_request(url: str, tags: Union[str, Iterable[str]]) \
+    -> Dict[str, Union[str, List[str]]]:
+    """ Scrape and parse xml retrieved from ``url``
+    and return parsed contents within ``tags``.
 
     Parameters
     ----------
     url: str
-    tags: str or Iterable[str]
-        A list of tags to extract (eg. `div`).
-    parser: a parser recognized by BeautifulSoup.
+    tags: str or an iterable of str
+        HTML tag(s) to retrieve the contents of.
 
     Returns
     -------
-    Dictionary containing tags as keys and parsed content as values.
+    A dictionary of ``tags`` as keys
+    and their contents (str or list of str) as values.
+
+    Raises
+    ------
+    HTML and parsing errors from ``requests`` and ``BeautifulSoup`` libraries.
 
     """
     contents = {}
     if isinstance(tags, str):
-        tags = [tags]
-    try:
-        raw_html = requests.get(url)
-        parse = BeautifulSoup(raw_html.content, 'html.parser')
-        for tag in tags:
-            data = parse.find_all(tag)
-            if len(set(data)) == 1:
-                contents[tag] = data[0].text
-            else:
-                contents[tag] = (item.text for item in data)
-    except requests.exceptions.RequestException:
-        raise
-    except Exception:
-        raise
+        tags = (tags,)
+    raw_html = requests.get(url)
+    parse = BeautifulSoup(raw_html.content, 'html.parser')
+    for tag in tags:
+        data = parse.find_all(tag)
+        if len(set(data)) == 1:
+            contents[tag] = data[0].text
+        else:
+            contents[tag] = [item.text for item in data]  # keep picklable
     return contents
 
+def esearch(query: str, db: str) -> Dict[str, Union[str, List[str]]]:
+    """ Perform an E-search.
+    Does not need to be called by the user, except perhaps to
+    verify the count.
 
-class DBQuery:
-    def __init__(self, query, database='pubmed', retmax=500):
-        """ Initialize a DBQuery object.
+    Parameters
+    ----------
+    query: str
+    db: str
+        A database, one of either "pubmed" or "pmc".
+
+    Returns
+    -------
+    dict with three keys: "webenv", "queryKey" and "count".
+        These are required for fetching summaries.
+
+    """
+    url = BASE_ESEARCH_URL + f'db={db}&term={query}&usehistory=y'
+    return process_request(url, ('webenv', 'queryKey', 'count'))
+
+def efetch(
+        db: str,
+        retmax: int,
+        webenv: str,
+        query_key: int,
+        retstart: int) \
+        -> str:
+    """ Perform an E-fetch. Scrape and parse summaries.
+    Does not need to be called by the user.
+
+    Parameters
+    ----------
+    db: str
+        A database, one of "pubmed" or "pmc".
+    retmax: int
+        Maximum number of summaries to request at one time.
+    webenv: str
+        Web environment flag.
+    query_key: int
+        Query key flag.
+    retstart: int
+        If the number of summaries to retrieve is large,
+        this can be used to retrieve summaries in batches.
+
+    Returns
+    -------
+    str
+        A concatenated list of strings as a single string.
+    
+    """
+    url = (BASE_EFETCH_URL + (f'db={db}&'
+                              f'retstart={retstart}&'
+                              f'retmax={retmax}&'
+                              f'WebEnv={webenv}&'
+                              f'query_key={query_key}&'
+                              f'retmode=xml'))
+    print(url)
+    contents = process_request(url, ('abstracttext',))
+    return '\n'.join(contents['abstracttext'])
+
+
+class DbQuery:
+    def __init__(self, query):
+        """ Initialize a DbQuery object.
 
         Parameters
         ----------
         query: str
-            A query (eg. 'breast cancer').
-        database: str
-            Database to query, one of either 'pubmed' or 'pmc'.
-        retmax: int
-            The maximum number of articles to scrape.
+            A query of interest, such as "breast cancer BRCA1".
 
         """
+        if isinstance(query, list):
+            self.query = '+'.join(query)
+        elif isinstance(query, str):
+            self.query = '+'.join(query.split(' '))
+            self.db = 'pubmed'
+            self.retmax = 500
 
-        if query == '':
-            raise ValueError('Provided empty string')
-
-        if database.lower() not in ('pubmed', 'pmc'):
-            raise ValueError(f'Unknown database: {database}')
-
-        self._db = database.lower()
-        self._retmax = int(retmax)
-        self._query = '+'.join(query.split(' '))
-        self._count = 0
-        self._lock = multiprocessing.Lock()
-
-    @property
-    def query(self):
-        return self._query
-
-    @functools.lru_cache(maxsize=1)
-    def _esearch(self):
-        """ Get PMIDs/UIDs and counts.
-
-        You should not need to call this method directly since
-        `efetch` calls this method automatically.
-
-        The results are cached so the method executes only once
-        in any case.
-
-        """
-        url = esearch_base + f'db={self._db}&term={self._query}&usehistory=y'
-        try:
-            contents = process_request(url, ['webenv', 'querykey', 'count'])
-        except Exception:
-            raise
-        self._count = contents['count']
-        return contents
-
-    def efetch(self, filename=''):
-        """ Scrape abstracts from open source articles and append
-        to a text (.txt) file.
+    def run(self, filename: str, parallel=True) -> IO[str]:
+        """ Initialize a DbQuery object.
 
         Parameters
         ----------
-        filename: str, optional argument
-            A .txt filename. The file may or may not exist.
+        filename: str
+            A filename/filepath to write summaries to.
+            Summaries are appended to a file if it exists.
+        parallel: bool
+            Use multiprocessing to fetch summaries in parallel.
 
         Returns
         -------
-        The filename to which data has been written/appended.
+        A string is written to a file.
 
         """
-        data = self._esearch()
-        webenv = data['webenv']
-        query_key = data['querykey']
-        retmax = max(self._retmax, 500)
+        t = time.time()
+        esearch_dict = esearch(self.query, self.db)
+        print('Completed searching for abstracts in '
+              f' {(time.time() - t):.2f} seconds.')
 
-        if not filename or not filename.endswith('.txt'):
-            filename = (self._query
-                        + datetime.date.today().strftime('%d%B%Y')
-                        + '.txt')
+        # multiprocessing setup
+        num_processes = 4 if parallel else 1
+        pool = mp.Pool(num_processes)
+        queue = list(range(0, int((esearch_dict['count'])[0]), self.retmax))
 
-        with open(filename, 'a') as file_handle:
-            count = int(self._count)
-            retstart = 0
-            while retstart < count:
-                url = (efetch_base
-                       + (f'db={self._db}&'
-                          f'retstart={retstart}&'
-                          f'retmax={retmax}&'
-                          f'WebEnv={webenv}&'
-                          f'query_key={query_key}&'
-                          f'retmode=xml'))
-                try:
-                    contents = process_request(url, 'abstracttext')
-                    file_handle.write('\n'.join(contents['abstracttext']))
-                except Exception:
-                    raise
-                retstart += retmax
-        return filename
+        # <queryKey><\queryKey> contains nothing if
+        # the number of abstracts is small
+        query_key = esearch_dict['queryKey'] if esearch_dict['queryKey'] else 1
+        efetch_ = functools.partial(
+            efetch,
+            self.db,
+            self.retmax,
+            esearch_dict['webenv'],
+            query_key)
 
-    def run(self, q):
-        """ Multiprocessing method """
-        data = self._esearch()
-        webenv = data['webenv']
-        query_key = data['query_key']
-        retmax = max(self._retmax, 500))
-        retstart = multiprocessing.Value('i', 0)
-        with self._lock.get_lock():
-            url = (efetch_base
-                    + (f'db={self._db}&'
-                        f'retstart={retstart}&'
-                        f'retmax={retmax}&'
-                        f'WebEnv={webenv}&'
-                        f'query_key={query_key}&'
-                        f'retmode=xml'))
-            retstart.value += retmax
-        
-        
-        t.join()
-        return
-        
-        
-
-    def __repr__(self):
-        return f'<class DBQuery, query={self._query}>, count={self._count}'
+        # main loop
+        with open(filename, 'a') as fhandle:
+            for string in pool.map(efetch_, queue):
+                fhandle.write(string)
+        print(f'Completed fetching in {(time.time() - t):.2f} seconds.')
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--query',
-        '-q',
-        help='Enter search terms to query Pubmed.')
+    parser = argparse.ArgumentParser(description='Perform an E-fetch')
+    parser.add_argument('query', nargs='+', type=str)
+    parser.add_argument('-f', default='')
     args = parser.parse_args()
-    dbquery = DBQuery(args.query)
-    dbquery.efetch('temp.txt')
